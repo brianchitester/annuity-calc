aca37db4099caa38c27a130bd82597d8939924bc
Brian Chitester <brian.chitester@dutchie.com>
2021-10-27 21:18:44 -0400

    no scale

--- components/header.tsx
+++ components/header.tsx
@@ -6,6 +6,10 @@ export const Header: FC = () => {
     <Head>
       <title>Annuity Calc</title>
       <meta name="description" content="calculate annuity costs" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+      />
       <link rel="icon" href="/favicon.ico" />
     </Head>
   );
